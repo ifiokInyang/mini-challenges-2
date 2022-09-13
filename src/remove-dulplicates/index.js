94bf6303d3a7557af31b1a4859ff6026a2ecfea7
ifiokInyang <80972231+ifiokInyang@users.noreply.github.com>
2022-09-13 16:45:06 +0100

    Update index.js
    Solution for the remove brackets task

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,3 +1,21 @@
-function removeDuplicates(obj) {}
-
+function removeDuplicates(obj) {
+let keyObject;
+let valueObject;
+let keyArray = Object.keys(obj)
+let sortedArray = keyArray.sort((a,b) => (a-b))
+let rearrangedObject = {}
+for (let i = 0; i<sortedArray.length; i++){
+keyObject = sortedArray[i]
+valueObject = obj[sortedArray[i]]
+rearrangedObject[keyObject] = valueObject
+// start from length-1
+valueObject.flat().filter(arrayElement => arrayElement.indexOf()> -1)
+}
+return rearrangedObject
+}
+console.log(removeDuplicates(
+{
+    "1": ["A", "B", "C"],
+    "2": ["A", "B", "D", "A"]
+  }))
 module.exports = removeDuplicates;
