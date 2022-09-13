afa7fda482b15f9bf0893b986717527785471d49
ifiokInyang <80972231+ifiokInyang@users.noreply.github.com>
2022-09-13 16:40:37 +0100

    Update index.js
    Solution to the isolate duplicates task

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,30 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    if(arguments.length <1 || typeof(text) != "string"){
+      throw "Please enter a valid string"
+    }
+    let newArray= []
+    let totalDuplicateCount= 0;
+    let totalCount= 0
+    let splitText= text.split('');
+    for(let i=0; i < splitText.length; i++){
+      newArray.push(splitText[i])
+      if(splitText[i+1] != null && splitText[i].toLowerCase() == splitText[i+1].toLowerCase()){
+        totalDuplicateCount++
+        if(totalDuplicateCount==2){
+          newArray.push('[')
+          totalCount++
+        }
+  
+      }else if(splitText[i+1] != null && splitText[i].toLowerCase() !== splitText[i+1].toLowerCase() || i == splitText.length-1){
+        if(totalDuplicateCount > 1){
+          newArray.push(']')
+        }
+        totalDuplicateCount= 0
+      }
+    }
+    return [newArray.join(''), totalCount]
+  }
+  console.log(isolateDuplicates("aaaabbcdefffffffg"))
+
 
 module.exports = isolateDuplicates;
